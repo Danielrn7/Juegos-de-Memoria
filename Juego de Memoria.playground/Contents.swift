4db94354fca7c9c90f9d1d2eafcc17752798e64c
Danielrn7 <31738492+Danielrn7@users.noreply.github.com>
2017-09-15 22:30:53 -0500

    Initial commit

---
+++ Juego de Memoria.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Juego de Memoria
+
+import UIKit
+
+
+
+var rango = 0...100
+
+
+// Iteración del rango
+for rango in 0...100 {
+    
+    //Números divisibles entre 5
+    if (rango % 5 == 0) {
+        print((rango), "Bingo!!!")
+    }
+    // Números pares
+    if (rango % 2 == 0){
+        print((rango), "par!!!")
+    }
+        // Números impares
+    else {
+        print((rango), "impar!!!")
+    };
+    // Números entre el 30 y 40
+    
+    if (rango >= 30 && rango <= 40) {
+        print((rango), "Viva Swift!!!")
+    }
+}
+
+
